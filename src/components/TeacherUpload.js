8b29d2736b316135b7a010d49f8df2c85f0c5eb0
Josh <dupetopetoman@gmail.com>
2023-05-13 16:45:46 +0200

    Done all of the main stuff, only viewport remaining

--- src/components/TeacherUpload.js
+++ src/components/TeacherUpload.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import firebase from "../firebaseConfig";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FileList from "./FileList";
 
 const TeacherUpload = ({ filetype, classx }) => {
+
+
   const [file, setFile] = useState(null);
   const [uploadProgress, setUploadProgress] = useState(0);
 
@@ -25,10 +28,7 @@ const TeacherUpload = ({ filetype, classx }) => {
 
   const handleUpload = async (e) => {
     e.preventDefault();
-
-    const nameInput = document.querySelector(
-      ".upld-input input[type=text]"
-    ).value;
+    const nameInput = document.querySelector(".upld-input input[type=text]");
 
     if (!file || !handleFileReq(file)) {
       alert("Can't upload this :(");
@@ -38,7 +38,7 @@ const TeacherUpload = ({ filetype, classx }) => {
     const storageRef = firebase.storage().ref();
     const folderPath = "Teacher-Uploads/" + classx + "/" + filetype + "/";
     const fileRef = storageRef.child(
-      `${folderPath}/${nameInput ? nameInput : file.name}`
+      `${folderPath}/${nameInput.value ? nameInput.value : file.name}`
     );
     const uploadTask = fileRef.put(file);
 
@@ -53,38 +53,43 @@ const TeacherUpload = ({ filetype, classx }) => {
         console.error("Error uploading file:", error);
       },
       () => {
-        alert("File uploaded successfully!");
         setFile(null);
+        nameInput.value = "";
         setUploadProgress(0);
+        alert("File uploaded successfully!");
       }
     );
   };
 
   return (
-    <form className="upld-form">
-      <div className="upld-form-group">
-        <label className="upld-input">
-          File name :
-          <input type="text" placeholder="(Leave blank for default)" />
-        </label>
-        <label id="file-upld">
-          Upload {filetype} :
-          <div>
-            <input
-              type="file"
-              onChange={handleFileChange}
-              className="choose-file-input"
-            />
-            <button onClick={(e) => handleUpload(e)} className="upld-button">
-              <FontAwesomeIcon icon="fa-solid fa-cloud-arrow-up" />
-            </button>
-            {uploadProgress > 0 && (
-              <p>Upload progress: {Math.round(uploadProgress)}%</p>
-            )}
+    <div id="teacher-uploads">
+      <form className="upld-form">
+        <div className="upld-form-group">
+          <p>Upload to {filetype} for {classx} :</p>
+          <label className="upld-input">
+            File name :
+            <input type="text" placeholder="(Leave blank for default)" />
+          </label>
+          <div id="teacher-file-upld">
+              <input
+                type="file"
+                onChange={handleFileChange}
+                className="choose-file-input"
+              />
+              <button onClick={(e) => handleUpload(e)} className="upld-button">
+                <FontAwesomeIcon icon="fa-solid fa-cloud-arrow-up" />
+              </button>
+              {uploadProgress > 0 && (
+                <p>Upload progress: {Math.round(uploadProgress)}%</p>
+              )}
           </div>
-        </label>
+        </div>
+      </form>
+      <div className="current-files">
+        <h3>Current files : </h3>
+        <FileList folderPath={`Teacher-Uploads/${classx}/${filetype}`} />
       </div>
-    </form>
+    </div>
   );
 };
 
